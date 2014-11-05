f8695079b73aa50460945c17c138bb42c29183f3
blackbaud-johnly <John.Lyons@blackbaud.com>
2014-11-05 17:10:35 -0500

    Add files and update readme

---
+++ CodeTableExampleJavaBizOps/src/codetableexamplejavabizops/CodeTableExampleJavaBizOps.java
@@ -0,0 +1,10 @@
+package codetableexamplejavabizops;
+
+public class CodeTableExampleJavaBizOps {
+
+    public static void main(String[] args) {
+        CodeTableExampleJFrame mainScreen = new CodeTableExampleJFrame();
+        mainScreen.setVisible(true);
+        mainScreen.setEnabled(true);
+    }
+}
